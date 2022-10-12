c22af4d5c46ab14d18ff0fd5b64b465f2b946e83
chickenchi <herosofthestom12@gmail.com>
2022-10-12 17:36:23 +0900

    8th commit

---
+++ BOJ2438.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++,puts(""))
+        for(int j=1;j<=i;j++)
+            printf("*");
+    return 0;
+}
