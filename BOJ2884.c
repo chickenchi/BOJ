9a80657a8d451521ee6eb0c5e9e769e56fe3edb1
chickenchi <herosofthestom12@gmail.com>
2022-10-17 08:08:51 +0900

    etc commit

---
+++ BOJ2884.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    a=a%10*100+a%100-a%10+a/100;
+    b=b%10*100+b%100-b%10+b/100;
+    printf("%d",a>b?a:b);
+	return 0;
+}
