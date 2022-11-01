8c8bb497de0f9f9e4d8eef04c38e35b6083cecdf
chickenchi <herosofthestom12@gmail.com>
2022-11-02 07:20:58 +0900

    edit wrong copied

--- BOJ11022.c
+++ BOJ11022.c
@@ -1,29 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct {
-    int x;
-    int y;
-} Cdn;
-
-int compare(const void *a, const void *b)
-{
-    if(((Cdn*)a) -> y < ((Cdn*)b) -> y) return -1;
-    else if(((Cdn*)a) -> y > ((Cdn*)b) -> y) return 1;
-    else if(((Cdn*)a) -> x < ((Cdn*)b) -> x) return -1;
-    else return 1;
-}
 
 int main()
 {
-    Cdn cdn[100001];
-    int a, i=-1;
+    int a,b,c;
     scanf("%d",&a);
-    while(i++<a-1)
-        scanf("%d %d",&cdn[i].x, &cdn[i].y);
-    qsort(cdn, a, sizeof(Cdn), compare);
-    i=-1;
-    while(i++<a-1)
-        printf("%d %d\n",cdn[i].x, cdn[i].y);
+    for(int i=1;i<=a;i++)
+    {
+        scanf("%d%d",&b,&c);
+        printf("Case #%d: %d + %d = %d\n",i,b,c,b+c);
+    }
     return 0;
 }
